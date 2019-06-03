c43a5bad609ece3af43e6647ab326ae142bcc88f
Bhargava Kunchanapalli <intuitivegrad30@gmail.com>
2019-06-03 12:50:16 -0700

    script.js update

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ $('.post-btn').click(function(){
   if($('#input_text').val() === ''){}
   else{
     const url = 'https://princeton-assgn-task2.herokuapp.com/sendmessage'
-    $.post(url, {'name': 'bhargava'}, function(data){
+    $.post(url, JSON.stringify({'name': 'bhargava'}), function(data){
       $('.display_messages .message_output p').text(data);
     });
   }
